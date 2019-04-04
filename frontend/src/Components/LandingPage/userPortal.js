9fe37d4731e406a10045cd52e7c55da881b7f248
Muna Sharma <munasharma@pursuit.org>
2019-04-04 17:55:40 -0400

    added icon thing

--- frontend/src/Components/LandingPage/userPortal.js
+++ frontend/src/Components/LandingPage/userPortal.js
@@ -5,8 +5,10 @@ const UserPortal = () => {
     <>
       <div className="userPortal">
         <h3> userPortal </h3>
-        <button> Log In</button>
-        <button> Sign Up </button>
+        <div className="">
+          <button className="logInBtn"> Log In</button>
+          <button className="logInBtn"> Sign Up </button>
+        </div>
       </div>
     </>
   );
