7cf9a309fa67ae6b6465016ff7909e7a82a5584e
abid <abidhussain@pursuit.org>
2019-04-04 17:07:44 -0400

    navbar first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import LandingPage from "./Components/landingPage";
+import Navbar from "./Components/Navbar/Navbar.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <LandingPage />
       </div>
     );
---
+++ frontend/src/Components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-parent">
+        <nav>
+          <NavLink to="/">Home</NavLink>{" "}
+          <NavLink to="/brooklyn">Brooklyn</NavLink>{" "}
+          <NavLink to="/queens">Queens</NavLink>{" "}
+          <NavLink to="/manhattan">Manhattan</NavLink>{" "}
+          <NavLink to="/bronx">Bronx</NavLink>{" "}
+          <NavLink to="/statenisland">Staten Island</NavLink>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
