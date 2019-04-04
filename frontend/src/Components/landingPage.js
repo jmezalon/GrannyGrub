5bedf02ddf39f3472d7d8fb0df8aecd4debba985
Muna Sharma <munasharma@pursuit.org>
2019-04-04 16:34:36 -0400

    working on landing page img

--- frontend/src/Components/landingPage.js
+++ frontend/src/Components/landingPage.js
@@ -4,7 +4,12 @@ class LandingPage extends React.Component {
   render() {
     return (
       <>
-        <div>Landing page ayooooooooo</div>
+        <div>Welcome to GrannyGrub!</div>
+        <img
+          src="https://cdn5.eyeem.com/thumb/cedbeecf8be48ed904d400f4e3767e43c29d2c20-1516553584991/w/900"
+          alt="granny being cute"
+          id="lpImg"
+        />
       </>
     );
   }
