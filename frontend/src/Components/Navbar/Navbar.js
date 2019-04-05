c5827a3d480b86159a688fe1abc75a1e37608829
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-04-05 13:08:06 -0400

    logged IN added in state

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import logo from "../../assets/logo.png";
 
 class Navbar extends Component {
   render() {
+    const { loggedIn } = this.props;
     return (
       <div className="navbar-parent">
         <nav>
@@ -14,28 +15,25 @@ class Navbar extends Component {
           </div>
         </nav>
 
-        <nav>
-          <div
-            className={
-              this.props.location.pathname !== "/"
-                ? "links-in-middle"
-                : "links-in-middle-null"
-            }
-          >
-            <NavLink to="/brooklyn">Brooklyn</NavLink>{" "}
-            <NavLink to="/queens">Queens</NavLink>{" "}
-            <NavLink to="/manhattan">Manhattan</NavLink>{" "}
-            <NavLink to="/bronx">Bronx</NavLink>{" "}
-            <NavLink to="/statenisland">Staten Island</NavLink>{" "}
-          </div>
-        </nav>
-
-        <nav>
-          <div className="auth-div">
-            <NavLink to="/auth/signup">Sign Up</NavLink>{" "}
-            <NavLink to="/auth/login">Log In</NavLink>
-          </div>
-        </nav>
+        {loggedIn ? (
+          <nav>
+            <div>
+              <NavLink to="/brooklyn">Brooklyn</NavLink>{" "}
+              <NavLink to="/queens">Queens</NavLink>{" "}
+              <NavLink to="/manhattan">Manhattan</NavLink>{" "}
+              <NavLink to="/bronx">Bronx</NavLink>{" "}
+              <NavLink to="/statenisland">Staten Island</NavLink>{" "}
+              <NavLink to="/">Log Out</NavLink>
+            </div>
+          </nav>
+        ) : (
+          <nav>
+            <div className="auth-div">
+              <NavLink to="/auth/signup">Sign Up</NavLink>{" "}
+              <NavLink to="/auth/login">Log In</NavLink>
+            </div>
+          </nav>
+        )}
       </div>
     );
   }
