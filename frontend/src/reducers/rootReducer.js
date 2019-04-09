26d5fc66d03a220237bb9c5fc4cb20e29a0e30cb
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-04-08 20:04:40 -0400

    we added a cuisine and having an error on home.js

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { grandmaReducer } from "./grandmaReducer";
+import { cuisineReducer } from "./cuisineReducer";
 
-const RootReducer = combineReducers({ grandmas: grandmaReducer });
+const RootReducer = combineReducers({
+  grandmas: grandmaReducer,
+  cuisines: cuisineReducer
+});
 
 export default RootReducer;
