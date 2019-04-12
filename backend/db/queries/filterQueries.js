fbe8f500f8f8d54f476bb8b7064654d4b34d4417
Muna Sharma <munasharma@pursuit.org>
2019-04-12 12:07:53 -0400

    filter by label done

--- backend/db/queries/filterQueries.js
+++ backend/db/queries/filterQueries.js
@@ -1,21 +1,21 @@
-const db = require('../connection');
+const db = require("../connection");
 
 const filterByCuisine = (req, res, next) => {
   cuisine_id = parseInt(req.params.id);
 
   db.any(
-    'SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1',
+    "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1",
     [cuisine_id]
   )
     .then(grandma => {
       res.status(200).json({
-        status: 'success',
+        status: "success",
         grandma: grandma,
-        message: 'filtered by cuisine',
+        message: "filtered by cuisine"
       });
     })
     .catch(err => {
-      console.log('error', err);
+      console.log("error", err);
       next(err);
     });
 };
@@ -44,23 +44,23 @@ const filterByLabel = (req, res, next) => {
   label_id = parseInt(req.params.id);
 
   db.any(
-    'SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1',
+    "SELECT label_name, labels.id AS label_id, dishes.id AS dish_id, dishes.user_id AS grandma_id, longitude, latitude, first_name, last_name, timeframe FROM label_dishes JOIN dishes ON label_dishes.dish_id = dishes.id JOIN users ON dishes.user_id = users.id JOIN labels ON label_dishes.label_id = labels.id WHERE label_dishes.label_id = $1",
     [label_id]
   )
     .then(grandma => {
       res.status(200).json({
-        status: 'success',
+        status: "success",
         grandma: grandma,
-        message: 'filtered by cuisine',
+        message: "filtered by cuisine"
       });
     })
     .catch(err => {
-      console.log('error', err);
+      console.log("error", err);
       next(err);
     });
 };
 
 module.exports = {
   filterByCuisine,
-  filterByLabel,
+  filterByLabel
 };
--- backend/routes/filter.js
+++ backend/routes/filter.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { filterByCuisine } = require('../db/queries/filterQueries.js');
+const {
+  filterByCuisine,
+  filterByLabel
+} = require("../db/queries/filterQueries.js");
 
-router.get('/cuisine/:id', filterByCuisine);
-router.get('/type/:id', filterByType);
+router.get("/cuisine/:id", filterByCuisine);
+router.get("/label/:id", filterByLabel);
 
 // router.post("/", postFavorite);
 // router.delete("/:id", deleteFavorite);
