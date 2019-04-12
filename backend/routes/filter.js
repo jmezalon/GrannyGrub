dd0612f89c70870416a4b212274c4ec28e8577b7
Muna Sharma <munasharma@pursuit.org>
2019-04-12 12:29:13 -0400

    filter by type is done

--- backend/routes/filter.js
+++ backend/routes/filter.js
@@ -1,14 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const {
   filterByCuisine,
-  filterByLabel
-} = require("../db/queries/filterQueries.js");
+  filterByLabel,
+  filterByType,
+} = require('../db/queries/filterQueries.js');
 
-router.get("/cuisine/:id", filterByCuisine);
-router.get("/label/:id", filterByLabel);
-
+router.get('/cuisine/:id', filterByCuisine);
+router.get('/label/:id', filterByLabel);
+router.get('/type/:id', filterByType);
 // router.post("/", postFavorite);
 // router.delete("/:id", deleteFavorite);
 
