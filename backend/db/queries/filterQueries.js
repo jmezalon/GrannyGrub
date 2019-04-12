1678bdebfa084c34488a861ede5f2c1c263e2ab9
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-04-12 09:57:15 -0400

    i just uncomment the filtered by type to fix the error on the backend

--- backend/db/queries/filterQueries.js
+++ backend/db/queries/filterQueries.js
@@ -1,61 +1,61 @@
-const db = require('../connection');
+const db = require("../connection");
 
 const filterByCuisine = (req, res, next) => {
   cuisine_id = parseInt(req.params.id);
 
   db.any(
-    'SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1',
+    "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1",
     [cuisine_id]
   )
     .then(grandma => {
       res.status(200).json({
-        status: 'success',
+        status: "success",
         grandma: grandma,
-        message: 'filtered by cuisine',
+        message: "filtered by cuisine"
       });
     })
     .catch(err => {
-      console.log('error', err);
+      console.log("error", err);
       next(err);
     });
 };
 
-// const filterByType = (req, res, next) => {
-//   cuisineId = parseInt(req.params.id);
-//
-//   db.any(
-//     "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1",
-//     [cuisineId]
-//   )
-//     .then(grandma => {
-//       res.status(200).json({
-//         status: "success",
-//         grandma: grandma,
-//         message: "filtered by cuisine"
-//       });
-//     })
-//     .catch(err => {
-//       console.log("error", err);
-//       next(err);
-//     });
-// };
+const filterByType = (req, res, next) => {
+  cuisineId = parseInt(req.params.id);
+
+  db.any(
+    "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1",
+    [cuisineId]
+  )
+    .then(grandma => {
+      res.status(200).json({
+        status: "success",
+        grandma: grandma,
+        message: "filtered by cuisine"
+      });
+    })
+    .catch(err => {
+      console.log("error", err);
+      next(err);
+    });
+};
 
 const filterByLabel = (req, res, next) => {
   label_id = parseInt(req.params.id);
 
   db.any(
-    'SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1',
+    "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1",
     [label_id]
   )
     .then(grandma => {
       res.status(200).json({
-        status: 'success',
+        status: "success",
         grandma: grandma,
-        message: 'filtered by cuisine',
+        message: "filtered by cuisine"
       });
     })
     .catch(err => {
-      console.log('error', err);
+      console.log("error", err);
       next(err);
     });
 };
@@ -63,4 +63,5 @@ const filterByLabel = (req, res, next) => {
 module.exports = {
   filterByCuisine,
   filterByLabel,
+  filterByType
 };
