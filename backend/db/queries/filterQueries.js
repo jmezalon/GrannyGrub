89bf2f35ba4bce5b4325c5874542bf62fd63bced
Muna Sharma <munasharma@pursuit.org>
2019-04-11 22:03:37 -0400

    changed db, added dummy cuisines, changed gmas names, added lableddish table and data, working on filter by label query

---
+++ backend/db/queries/filterQueries.js
@@ -0,0 +1,66 @@
+const db = require('../connection');
+
+const filterByCuisine = (req, res, next) => {
+  cuisine_id = parseInt(req.params.id);
+
+  db.any(
+    'SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1',
+    [cuisine_id]
+  )
+    .then(grandma => {
+      res.status(200).json({
+        status: 'success',
+        grandma: grandma,
+        message: 'filtered by cuisine',
+      });
+    })
+    .catch(err => {
+      console.log('error', err);
+      next(err);
+    });
+};
+
+// const filterByType = (req, res, next) => {
+//   cuisineId = parseInt(req.params.id);
+//
+//   db.any(
+//     "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1",
+//     [cuisineId]
+//   )
+//     .then(grandma => {
+//       res.status(200).json({
+//         status: "success",
+//         grandma: grandma,
+//         message: "filtered by cuisine"
+//       });
+//     })
+//     .catch(err => {
+//       console.log("error", err);
+//       next(err);
+//     });
+// };
+
+const filterByLabel = (req, res, next) => {
+  label_id = parseInt(req.params.id);
+
+  db.any(
+    'SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name,  timeframe, cuisines.id AS cuisine_Id FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.cuisine_id = $1',
+    [label_id]
+  )
+    .then(grandma => {
+      res.status(200).json({
+        status: 'success',
+        grandma: grandma,
+        message: 'filtered by cuisine',
+      });
+    })
+    .catch(err => {
+      console.log('error', err);
+      next(err);
+    });
+};
+
+module.exports = {
+  filterByCuisine,
+  filterByLabel,
+};
