2c3620a9ce1d64a576e023617558f812d6a9f973
Muna Sharma <munasharma@pursuit.org>
2019-04-15 12:24:15 -0400

    fixed filter quiries

--- backend/db/queries/filterQueries.js
+++ backend/db/queries/filterQueries.js
@@ -7,10 +7,10 @@ const filterByCuisineMap = (req, res, next) => {
     "SELECT users.id AS user_id, longitude, latitude, first_name, last_name, profile_pic, cuisines.type AS cuisine_type FROM users LEFT JOIN cuisines ON cuisines.id = users.cuisine_id WHERE users.cuisine_id = $1 GROUP BY users.id, longitude, latitude, first_name, last_name, cuisines.type",
     [cuisine_id]
   )
-    .then(grandma => {
+    .then(grandmas => {
       res.status(200).json({
         status: "success",
-        grandma: grandma,
+        grandma: grandmas,
         message: "filtered by cuisine"
       });
     })
@@ -24,13 +24,13 @@ const filterByCuisine = (req, res, next) => {
   cuisine_id = parseInt(req.params.id);
 
   db.any(
-    "SELECT users.id AS user_id, longitude, latitude, first_name, last_name, ARRAY_AGG(dishes.timeframe) AS timeframes, cuisines.type AS cuisine_type, ARRAY_AGG(dishes.type) AS dish_type FROM users LEFT JOIN dishes ON users.id = dishes.user_id JOIN cuisines ON cuisines.id = users.cuisine_id WHERE users.cuisine_id = $1 GROUP BY users.id, longitude, latitude, first_name, last_name, cuisines.type",
+    "SELECT users.id AS user_id, longitude, latitude, first_name, last_name,  profile_pic, ARRAY_AGG(dishes.timeframe) AS timeframes, cuisines.type AS cuisine_type, ARRAY_AGG(dishes.type) AS dish_type FROM users LEFT JOIN dishes ON users.id = dishes.user_id JOIN cuisines ON cuisines.id = users.cuisine_id WHERE users.cuisine_id = $1 GROUP BY users.id, longitude, latitude, first_name, last_name, cuisines.type",
     [cuisine_id]
   )
-    .then(grandma => {
+    .then(grandmas => {
       res.status(200).json({
         status: "success",
-        grandma: grandma,
+        grandma: grandmas,
         message: "filtered by cuisine"
       });
     })
@@ -45,13 +45,13 @@ const filterByType = (req, res, next) => {
 
   if (dish_type === 0) {
     db.any(
-      "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name, timeframe, cuisines.id AS cuisine_Id, dishes.type AS dish_type FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.type = 'sit-down'",
+      "SELECT users.id AS user_id, longitude, latitude, first_name, last_name, profile_pic,  ARRAY_AGG(dishes.timeframe) AS timeframes, cuisines.type AS cuisine_type, dishes.type AS dish_type, ARRAY_AGG(dishes.type) AS dish_type FROM users LEFT JOIN dishes ON users.id = dishes.user_id JOIN cuisines ON cuisines.id = users.cuisine_id WHERE dishes.type= 'sit-down' GROUP BY users.id, longitude, latitude, first_name, last_name,dishes.type, cuisines.type",
       [dish_type]
     )
-      .then(grandma => {
+      .then(grandmas => {
         res.status(200).json({
           status: "success",
-          grandma: grandma,
+          grandma: grandmas,
           message: "filtered by cuisine"
         });
       })
@@ -61,13 +61,13 @@ const filterByType = (req, res, next) => {
       });
   } else if (dish_type === 1) {
     db.any(
-      "SELECT dishes.id AS dish_id, cuisines.type AS cuisine_type, longitude, latitude, first_name, last_name, timeframe, cuisines.id AS cuisine_Id, dishes.type AS dish_type FROM users JOIN cuisines ON users.cuisine_id = cuisines.id JOIN dishes ON dishes.user_id = users.id WHERE dishes.type = 'pick-up'",
+      "SELECT users.id AS user_id, longitude, latitude, first_name, last_name, profile_pic, ARRAY_AGG(dishes.timeframe) AS timeframes, cuisines.type AS cuisine_type, dishes.type AS dish_type, dishes.type AS dish_type FROM users LEFT JOIN dishes ON users.id = dishes.user_id JOIN cuisines ON cuisines.id = users.cuisine_id WHERE dishes.type= 'pick-up' GROUP BY users.id, longitude, latitude, first_name, last_name,dishes.type, cuisines.type",
       [dish_type]
     )
-      .then(grandma => {
+      .then(grandmas => {
         res.status(200).json({
           status: "success",
-          grandma: grandma,
+          grandma: grandmas,
           message: "filtered by cuisine"
         });
       })
@@ -82,7 +82,7 @@ const filterByLabel = (req, res, next) => {
   label_id = parseInt(req.params.id);
 
   db.any(
-    "SELECT label_name, labels.id AS label_id, dishes.id AS dish_id, dishes.user_id AS grandma_id, longitude, latitude, first_name, last_name,dishes.type AS dish_type, timeframe FROM label_dishes JOIN dishes ON label_dishes.dish_id = dishes.id JOIN users ON dishes.user_id = users.id JOIN labels ON label_dishes.label_id = labels.id WHERE label_dishes.label_id = $1",
+    "SELECT label_name, labels.id AS label_id, dishes.id AS dish_id, dishes.user_id AS grandma_id,  profile_pic,longitude, latitude, first_name, last_name,dishes.type AS dish_type, timeframe FROM label_dishes JOIN dishes ON label_dishes.dish_id = dishes.id JOIN users ON dishes.user_id = users.id JOIN labels ON label_dishes.label_id = labels.id WHERE label_dishes.label_id = $1",
 
     [label_id]
   )
