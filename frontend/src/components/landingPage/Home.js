2c536d71b82b4f49031412b86076d6b2b4d1c4c3
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-04-15 15:52:20 -0400

    add

---
+++ frontend/src/components/landingPage/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    // const allCuisines = props.cuisines((food, i) => {
+    //   return (
+    //     <div key={food.id}>
+    //       <h3>{food.type}</h3>
+    //     </div>
+    //   );
+    // });
+    return (
+      <>
+        <div className="home">
+          <form>
+            <input
+              className="searchbar"
+              type="text"
+              placeholder="Search for a cuisine"
+            />
+            <button>Search</button>
+          </form>
+          <div>
+            <p>we will have a cuisine component in this div</p>
+          </div>
+          <div>
+            <h5>
+              this is the component that will hold the map, just like we have in
+              wireframe, mapview.png
+            </h5>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Home;
---
+++ frontend/src/components/landingPage/landingPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import UserPortal from "./userPortal";
+import CookPortal from "./cookPortal";
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="landingPage">
+          <h1> Welcome to GrannyGrub! </h1>
+          <div className="middle-landing">
+            <div className="middle-landing-left">
+              <div className="aboutUs">
+                <h3>About us</h3>
+                <p>
+                  Giving New Yorkers authentic home made taste that only comes
+                  from years of wisdom and experience. Our Grannies offer dishes
+                  that you won’t find elsewhere. We are confident that these are
+                  some of the best meals you’ll ever have.
+                </p>
+              </div>
+              <div>
+                <Link to="/mainpage">
+                  <button id="taste-button"> Get a taste </button>
+                </Link>
+              </div>
+              <CookPortal />
+            </div>
+            <img
+              src="https://cdn.dribbble.com/users/28275/screenshots/5369506/shostudio_grandma-1.gif"
+              alt="granny being cute"
+              id="lpImg"
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default LandingPage;
