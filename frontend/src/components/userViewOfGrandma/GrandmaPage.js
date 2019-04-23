dccbc06a4acd09303b384efa8470bc4cbab9865b
Muna Sharma <munasharma@pursuit.org>
2019-04-23 12:56:09 -0400

    set default to first dish type that is true, added a message if no dishes are found

--- frontend/src/components/userViewOfGrandma/GrandmaPage.js
+++ frontend/src/components/userViewOfGrandma/GrandmaPage.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "../../css/userViewOfGrandma/GrandmaPage.css";
-import GrandmasDishes from "../dishes/dishesUserView";
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../../css/userViewOfGrandma/GrandmaPage.css';
+import GrandmasDishes from '../dishes/dishesUserView';
 
 class GrandmaPage extends Component {
   state = {
-    type: "pick-up",
-    selectedDish: []
+    type: '',
+    selectedDish: [],
   };
 
   componentDidMount() {
@@ -15,9 +15,18 @@ class GrandmaPage extends Component {
     this.props.getGrandmasDishes(id);
   }
 
+  componentDidUpdate(prevProps) {
+    let defaultDishes = prevProps.dishes;
+    if (!defaultDishes.length && this.props.dishes.length) {
+      this.setState({
+        type: this.props.dishes[0].type,
+      });
+    }
+  }
+
   handleTypeToggle = e => {
     this.setState({
-      type: e.target.value
+      type: e.target.value,
     });
   };
 
@@ -26,12 +35,13 @@ class GrandmaPage extends Component {
   };
 
   render() {
-    console.log(this.state.selectedDish);
-    let { grandma } = this.props;
+    // console.log(this.state.selectedDish);
+
+    let { grandma, dishes } = this.props;
     if (!Object.values(grandma).length) return null;
 
     let grannyId = this.props.match.params.id;
-
+    console.log('THE GRANDMA STATE', this.state);
     return (
       <div className="user-view-granny-page">
         <label htmlFor="pick-up"> pickup </label>
