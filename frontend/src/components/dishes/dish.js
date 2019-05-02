b71cc1dfb595555220f6ef5a52d9b791b1ff540d
Muna Sharma <munasharma@pursuit.org>
2019-05-02 13:17:07 -0400

    css for dishes done with grid

--- frontend/src/components/dishes/dish.js
+++ frontend/src/components/dishes/dish.js
@@ -1,56 +1,55 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 const Dish = ({ dish, handleClick }) => {
   return (
     <div className="dish-view" key={dish.dish_id}>
-      <p id="tag">dishinfo</p>
-
-      <div className="dishInfo">
+      <div className="dish-left-side">
         <div id="dish-header">
           <h2> {dish.name}</h2>
           <p> ${dish.price} </p>
         </div>
 
-        <p> Available for {dish.type} </p>
+        <div className="dishInfo">
+          <p> Available for {dish.type} </p>
 
-        <p> On {dish.date.slice(0, 10)} </p>
-        <p> For {dish.timeframe} </p>
-      </div>
+          <p> On {dish.date.slice(0, 10)} </p>
+          <p> For {dish.timeframe} </p>
+        </div>
 
-      <p id="tag"> btn&amount </p>
+        <div id="dish-bottom-left">
+          <Link
+            to={
+              dish.remaining_quantity !== 0
+                ? `/order/dish/${dish.dish_id}`
+                : null
+            }
+          >
+            <button>
+              {' '}
+              {dish.remaining_quantity === 0
+                ? 'sold out'
+                : dish.type === 'sit-down'
+                ? 'book'
+                : 'order'}{' '}
+            </button>
+          </Link>
 
-      <div id="dish-bottom-left">
-        <Link
-          to={
-            dish.remaining_quantity !== 0 ? `/order/dish/${dish.dish_id}` : null
-          }
-        >
-          <button>
-            {" "}
-            {dish.remaining_quantity === 0
-              ? "sold out"
-              : dish.type === "sit-down"
-              ? "book"
-              : "order"}{" "}
-          </button>
-        </Link>
-
-        <div id="dish-quantity">
-          <p>
-            {" "}
-            remaining{" "}
-            {dish.remaining_quantity === null
-              ? dish.quantity
-              : dish.remaining_quantity}{" "}
-          </p>
-          <p>{dish.quantity} </p>
+          <div id="dish-quantity">
+            <p>
+              {' '}
+              remaining{' '}
+              {dish.remaining_quantity === null
+                ? dish.quantity
+                : dish.remaining_quantity}{' '}
+            </p>
+            <p>{dish.quantity} </p>
+          </div>
         </div>
       </div>
-      <p id="tag"> btn&amount </p>
       <div className="dish-right-side">
-        <img src={dish.img_url} alt="dish" id="dish_img" />
-        <p> lables go here </p>
+        <img src={dish.img_url} alt="dish" className="dish_img" />
+        <p id="dish-lables"> lables go here </p>
       </div>
     </div>
   );
