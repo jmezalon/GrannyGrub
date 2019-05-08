f72412ee998546f5f469e69af2c27d00f8608d59
Muna Sharma <munasharma@pursuit.org>
2019-05-08 12:09:46 -0400

    changed the users and dishes  table in the seed file and changed the all users queries

--- frontend/src/components/userViewOfGrandma/Order.js
+++ frontend/src/components/userViewOfGrandma/Order.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Link, withRouter, Redirect } from "react-router-dom";
 import { Modal } from "./Modal.js";
-// grandma/7
+
 class Order extends React.Component {
   render() {
     const { dish, count } = this.props;
     let price = (dish.price * count).toFixed(2);
     return (
-
       <>
         <button className="back-to-grandma">
           <Link className="grandma-link" to={`/grandma/${dish.user_id}`}>
             Back to Grandma's Page
           </Link>
         </button>
+
         <Modal>
           <div>
             <div id="dish-image">
@@ -21,53 +21,53 @@ class Order extends React.Component {
             </div>
 
             <div className="dish-info">
-              <div id="name-price-time">
+              <div id="order-header">
                 <p id="dish-name">{dish.name}</p>
-                <p className="checkout-order-info">Amount: ${price}</p>
-                <p className="checkout-order-info">
-                  Order Type: {dish.timeframe}{" "}
-                  {dish.type === "pick-up" ? "for" : "to"}{" "}
-                  {dish.type === "pick-up" ? "Pick-up" : "Stay"}
-                </p>
+                <p> ${dish.price} </p>
               </div>
-              <div className="dish-right-sie">
-                <div id="dish-label">
-                  <div className="plus-minus-button">
-                    <label>Quantity: </label>
-                    <i
-                      onClick={this.props.handleSubChange}
-                      className="fas fa-minus-square"
-                    />{" "}
-                    {this.props.count}{" "}
-                    <i
-                      onClick={this.props.handleAddChange}
-                      className="fas fa-plus-square"
-                    />
-                  </div>
-                </div>
-                <div id="dish-desc">
-                  <label>Description: </label>
-                  <p>{dish.description}</p>
-                </div>
 
-                <div id="dish-checkout">
-                  <Link
-                    to={
-                      dish.remaining_quantity !== 0
-                        ? `/order/dish/${dish.id}/checkout`
-                        : `/grandma/${dish.user_id}`
-                    }
+              <div id="dish-desc">
+                <label>Description: </label>
+                <p>{dish.description}</p>
+              </div>
+
+              <p className="checkout-order-info">
+                Order Type: {dish.timeframe}{" "}
+                {dish.type === "pick-up" ? "for" : "to"}{" "}
+                {dish.type === "pick-up" ? "Pick-up" : "Stay"}
+              </p>
+
+              <div className="plus-minus-button">
+                <label>Quantity: </label>
+                <i
+                  onClick={this.props.handleSubChange}
+                  className="fas fa-minus-square"
+                />{" "}
+                {this.props.count}{" "}
+                <i
+                  onClick={this.props.handleAddChange}
+                  className="fas fa-plus-square"
+                />
+                <p className="checkout-order-info">Order Total: ${price} </p>
+              </div>
+
+              <div id="dish-checkout">
+                <Link
+                  to={
+                    dish.remaining_quantity !== 0
+                      ? `/order/dish/${dish.id}/checkout`
+                      : `/grandma/${dish.user_id}`
+                  }
+                >
+                  <button
+                    id="dish-checkout-button"
+                    onClick={this.props.handleCheckOutClick}
                   >
-                    <button
-                      id="dish-checkout-button"
-                      onClick={this.props.handleCheckOutClick}
-                    >
-                      {dish.remaining_quantity === 0
-                        ? "back to grandma"
-                        : "Checkout"}
-                    </button>
-                  </Link>
-                </div>
+                    {dish.remaining_quantity === 0
+                      ? "back to grandma"
+                      : "Checkout"}
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
