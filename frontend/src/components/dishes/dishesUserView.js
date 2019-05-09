eecca47cbd32273173c703321dafebcc9b6c2061
abid <abidhussain@pursuit.org>
2019-05-09 09:36:23 -0400

    remove pick-up/sit-down toggle from userViewOfGrandma

--- frontend/src/components/dishes/dishesUserView.js
+++ frontend/src/components/dishes/dishesUserView.js
@@ -7,74 +7,46 @@ class GrandmasDishes extends React.Component {
     sitdowndisplay: true
   };
 
-  componentDidMount = () => {
-    const resultDishes = this.filterDishes();
-    if (!resultDishes.length) {
-      this.setState({
-        sitdowndisplay: false
-      });
-    }
-  };
+  // componentDidMount = () => {
+  //   const resultDishes = this.filterDishes();
+  //   if (!resultDishes.length) {
+  //     this.setState({
+  //       sitdowndisplay: false
+  //     });
+  //   }
+  // };
 
-  filterDishes = () => {
-    const filteredDishes = this.props.dishes.filter(dish => {
-      // return (i.key = i);
-      if (this.state.sitdowndisplay) {
-        return dish.type === "sit-down";
-      } else {
-        return dish.type === "pick-up";
-      }
-    });
+  // filterDishes = () => {
+  //   const filteredDishes = this.props.dishes.filter(dish => {
+  //     // return (i.key = i);
+  //     if (this.state.sitdowndisplay) {
+  //       return dish.type === "sit-down";
+  //     } else {
+  //       return dish.type === "pick-up";
+  //     }
+  //   });
+  //
+  //   return filteredDishes;
+  // };
 
-    return filteredDishes;
-  };
-
-  handleTypeToggle = e => {
-    const value = e.target.value;
-    this.setState(prevState => {
-      return {
-        type: value,
-        sitdowndisplay: !prevState.sitdowndisplay
-      };
-    });
-  };
+  // handleTypeToggle = e => {
+  //   const value = e.target.value;
+  //   this.setState(prevState => {
+  //     return {
+  //       type: value,
+  //       sitdowndisplay: !prevState.sitdowndisplay
+  //     };
+  //   });
+  // };
 
   render() {
-    const resultDishes = this.filterDishes();
+    // const resultDishes = this.filterDishes();
+    let dishes = this.props.dishes;
+    if (!dishes.length) return null;
     return (
       <>
-        <div id="dish-btns-container">
-          <label htmlFor="pick-up" />
-          <button
-            name="type"
-            value="pick-up"
-            onClick={this.handleTypeToggle}
-            checked={!this.state.sitdowndisplay}
-            className={
-              !this.state.sitdowndisplay ? "filter-btn-selected" : "filter-btn"
-            }
-          >
-            {" "}
-            Pick Up{" "}
-          </button>
-          <label htmlFor="sit-down" />
-          <button
-            name="type"
-            value="sit-down"
-            onClick={this.handleTypeToggle}
-            checked={this.state.sitdowndisplay}
-            className={
-              this.state.sitdowndisplay ? "filter-btn-selected" : "filter-btn"
-            }
-          >
-            Sit Down{" "}
-          </button>
-        </div>
         <div id="dish-display-main">
-          <DishList
-            dishes={resultDishes}
-            handleClick={this.props.handleClick}
-          />
+          <DishList dishes={dishes} handleClick={this.props.handleClick} />
         </div>
       </>
     );
@@ -82,3 +54,31 @@ class GrandmasDishes extends React.Component {
 }
 
 export default withRouter(GrandmasDishes);
+
+// <div id="dish-btns-container">
+//   <label htmlFor="pick-up" />
+//   <button
+//     name="type"
+//     value="pick-up"
+//     onClick={this.handleTypeToggle}
+//     checked={!this.state.sitdowndisplay}
+//     className={
+//       !this.state.sitdowndisplay ? "filter-btn-selected" : "filter-btn"
+//     }
+//   >
+//     {" "}
+//     Pick Up{" "}
+//   </button>
+//   <label htmlFor="sit-down" />
+//   <button
+//     name="type"
+//     value="sit-down"
+//     onClick={this.handleTypeToggle}
+//     checked={this.state.sitdowndisplay}
+//     className={
+//       this.state.sitdowndisplay ? "filter-btn-selected" : "filter-btn"
+//     }
+//   >
+//     Sit Down{" "}
+//   </button>
+// </div>
