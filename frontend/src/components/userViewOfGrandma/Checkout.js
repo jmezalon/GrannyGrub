3fa2471388c5da87eff0bb3b70c05bfdd5822cd9
abid <abidhussain@pursuit.org>
2019-05-14 11:54:29 -0400

    back arrow at checkout

--- frontend/src/components/userViewOfGrandma/Checkout.js
+++ frontend/src/components/userViewOfGrandma/Checkout.js
@@ -17,57 +17,61 @@ const Checkout = ({
   confirmation,
   handleUserSignUpType,
   currentUser
-
 }) => {
   console.log("here");
 
   return (
-    <Modal>
-      <div className="info-order">
-        <div id="dish-image-order">
-          <img src={dish.img_url} alt="dish" />
-        </div>
+    <div className="xyz">
+      <Link to={`/order/dish/${parseInt(dish.id)}`} id="back-link-order">
+        <i className="fas fa-arrow-left" id="order-back-arrow" />
+      </Link>
+      <Modal>
+        <div className="info-order">
+          <div id="dish-image-order">
+            <img src={dish.img_url} alt="dish" />
+          </div>
 
-        <div id="checkout-left-side">
-          <h3> Order details: </h3>
-          <p id="checkout-dish-name">
-            {count} X {dish.name}
-            {count > 1 ? "s" : ""}
-          </p>
-
-          <div className="checkout-order-info">
-            <p>Order Total: ${price}</p>
-            <p>
-              {order_type} Time:{" "}
-              {dish.timeframe === "Lunch" ? "12:00pm-2:00pm" : "6:00pm-8:00pm"}
+          <div id="checkout-left-side">
+            <h3> Order details: </h3>
+            <p id="checkout-dish-name">
+              {count} X {dish.name}
+              {count > 1 ? "s" : ""}
             </p>
+
+            <div className="checkout-order-info">
+              <p>Order Total: ${price}</p>
+              <p>
+                {order_type} Time:{" "}
+                {dish.timeframe === "Lunch"
+                  ? "12:00pm-2:00pm"
+                  : "6:00pm-8:00pm"}
+              </p>
+            </div>
+
+            <div id="checkout-edit">
+              <p> Need to make changes to this order? </p>
+              <Link to={`/order/dish/${parseInt(dish.id)}`}>
+                <button id="edit-button">edit</button>
+              </Link>
+            </div>
           </div>
 
-          <div id="checkout-edit">
-            <p> Need to make changes to this order? </p>
-            <Link to={`/order/dish/${parseInt(dish.id)}`}>
-              <button id="edit-button">edit</button>
-            </Link>
+          <div id="checkout-right-side">
+            <div>
+              <OrderForm
+                order_type={order_type}
+                full_name={full_name}
+                dish={dish}
+                count={count}
+                address={address}
+                currentUser={currentUser}
+                handleUserSignUpType={handleUserSignUpType}
+              />
+            </div>
           </div>
         </div>
-
-        <div id="checkout-right-side">
-          <div>
-            <OrderForm
-              order_type={order_type}
-              full_name={full_name}
-              dish={dish}
-              count={count}
-              address={address}
-
-              currentUser={currentUser}
-
-              handleUserSignUpType={handleUserSignUpType}
-            />
-          </div>
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+    </div>
   );
 };
 
